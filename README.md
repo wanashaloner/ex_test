7c9e1927adb53d04222f3aaeb885297a710e2158
wanashaloner <wanashaloner@163.com>
2019-01-08 00:09:45 +0800

    初始化 Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Project 'ex_test' is a test job for Express learnning.
+	2019-1-7 23:50:40 writed by w.l.
+
+	
---
+++ app.js
@@ -0,0 +1,56 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+//delete by w.l   var indexRouter = require('./routes/index');
+//delete by w.l   var usersRouter = require('./routes/users');
+
+var app = express();
+
+//Require file system module by w.l
+var fs = require('file-system');
+
+//Include controllers by w.l
+fs.readdirSync('controllers').forEach(function (file) {
+	if(file.substr(-3) == '.js'){
+		const route = require('./controllers/' + file);
+		route.controller(app);
+	}
+});
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//delete by w.l   app.use('/', indexRouter);
+//delete by w.l   app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+app.listen(3000, function() { console.log('listening on 3000') });
+
+module.exports = app;
